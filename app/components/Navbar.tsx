8243314ee85e2b72ded7d90a822a9a98da03defe
Alberto Moreno <alberto.moreno.sa@gmail.com>
2026-08-08 17:55:54 -0600

    feat(navbar) add rebforma item (#20)

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,9 +6,10 @@ const navLinks = [
   { label: "Experience", href: "#experience", num: "02" },
   { label: "Projects", href: "#projects", num: "03" },
   { label: "Skills", href: "#skills", num: "04" },
-  { label: "Taco", href: "#taco", num: "05" },
-  { label: "Testimonials", href: "#testimonials", num: "06" },
-  { label: "Contact", href: "#contact", num: "07" },
+  { label: "Reforma", href: "#reforma", num: "06" },
+  { label: "Taco", href: "#taco", num: "07" },
+  { label: "Testimonials", href: "#testimonials", num: "08" },
+  { label: "Contact", href: "#contact", num: "09" },
 ];
 
 const scrollTo = (e: React.MouseEvent<HTMLAnchorElement>, id: string) => {
